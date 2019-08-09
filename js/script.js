b63919937b084459b1065b40b7ff141ce4223fb2
vi7ali <vi7ali@gmail.com>
2019-08-09 18:50:58 +0300

    JS new function to display add book container

--- js/script.js
+++ js/script.js
@@ -15,26 +15,26 @@ let myLibrary = [{title: 'Book1', author: 'Author1', pages: 100, status: 'read'}
              {title: 'Book4', author: 'Author4', pages: 103, status: 'read'},
              {title: 'Book5', author: 'Author5', pages: 104, status: 'read'}];
 
-addButton.addEventListener('click', addBook);
+addButton.addEventListener('click', displayAddBookDiv);
 
-function addBook() {    
-    for( let i = 0; i < 5; i++ ) {
-        addToGrid(createDiv(`${i}`));
-    }
+function displayAddBookDiv() {    
+    let addBookContainer = document.createElement('div');
+    addBookContainer.setAttribute('class', 'addbook-container');
+    
 }
 
 function render(){
     myLibrary.forEach( (book) => {
-        for ( let key in book ) addToGrid(createDiv(book[key]));
-        addToGrid(createDiv('edit'));
+        for ( let key in book ) addToGrid(createGridDiv(book[key]));
+        addToGrid(createGridDiv('edit'));
     });
 }
 
-function addToGrid(newDiv){    
+function addToGrid(newDiv){
     gridContainer.appendChild(newDiv);
 }
 
-function createDiv(content) {
+function createGridDiv(content) {
     let newDiv = document.createElement('div');
     let newContent = document.createTextNode(content);
     newDiv.appendChild(newContent);
